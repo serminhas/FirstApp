8d2b95106ce56b3c5fb61b83f07b726dc6663824
serminhas <serminerten@gmail.com>
2019-10-26 12:27:41 +0300

    ilk proje eklendi

---
+++ FirstApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Adinizi Giriniz");
+            Console.WriteLine("Giriş yapan kullanici: " + Console.ReadLine());
+
+            Console.Read();
+
+        }
+
+    }
+}
